29722fcc21b6619066d9b071991695b1c4adc9a4
chad svastisalee <swasgrafx@gmail.com>
2021-02-18 16:12:43 -0600

    Complete Posts CRUD

---
+++ core/layout.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <title>Hello, I am MicroTrain2101</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="Microtrain2101 home page">
+    <meta name="keywords" content="full stack, web developer, full stack web developer, CSM, Scrum Master, home page">
+    <link rel="stylesheet" type="text/css" href="./dist/css/main.min.css">
+    <link rel="apple-touch-icon" sizes="180x180" href="apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="favicon-16x16.png">
+    <link rel="manifest" href="site.webmanifest">
+  </head>
+  <body>
+<header>
+  <span class="logo">MicroTrain2101</span>
+  <a id="toggleMenu">Menu<a>
+    <nav>
+        <ul>
+          <li><a href="index.html">Home</a></li>
+          <li><a href="resume.html">Resume</a></li>
+          <li><a href="contact.html">Contact</a></li>
+        </ul>
+      </nav>
+</header>
+
+        <div class="row">
+            <div id="Content">
+                <?php echo $content; ?>
+            </div>
+            <div id="Sidebar">
+              <div id="AboutMe">
+                <div class="header">Hello, I am MicroTrain2101</div>
+                <img src="https://www.gravatar.com/avatar/4678a33bf44c38e54a58745033b4d5c6?d=mm" alt="My Avatar" class="img-circle">
+              </div>
+            </div>
+        </div>
+
+        <div id="Footer" class="clearfix">
+            <small>&copy; 2017 - MyAwesomeSite.com</small>
+            <ul role="navigation">
+                <li><a href="terms.html">Terms</a></li>
+                <li><a href="privacy.html">Privacy</a></li>
+            </ul>
+        </div>
+    </div>
+
+  </body>
+
+</html>
---
+++ public/posts/view.php
@@ -0,0 +1,26 @@
+<?php
+include '../../core/db_connect.php';
+
+$input = filter_input_array(INPUT_GET);
+$slug = preg_replace("/[^a-z0-9-]+/", "", $input['slug']);
+
+$content=null;
+$stmt = $pdo->prepare('SELECT * FROM posts WHERE slug = ?');
+$stmt->execute([$slug]);
+$row = $stmt->fetch();
+
+
+$content= <<<EOT
+<h1>{$row['title']}</h1>
+{$row['body']}<br>
+{$row['meta_description']}<br>
+{$row['meta_keywords']}<br>
+{$row['created']}
+<div>
+<a class="btn btn-link" href="edit.php?id={$row['id']}">Edit</a>
+<a class="btn btn-link" href="delete.php?id={$row['id']}">Delete</a>
+</div>
+EOT;
+echo $content;
+
+// include '../../core/layout.php';
